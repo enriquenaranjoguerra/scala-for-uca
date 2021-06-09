2e02e733e82180e6c9f4ff2851adf29699f7a69c
Enrique <kikekikee@hotmail.com>
2021-06-09 14:59:02 +0200

    string format

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,9 @@
 object Main extends App {
-  println("Hello, World!")
+  var x = 3
+  var y = 4
+  // incrementamos el valor de y en 2
+  y += 2
+  val suma = x + y
+  val mensaje = "La suma de %d más %d es %d".format(x, y, suma)
+  println(mensaje)
 }
